7f3eae3c87fd9494495d8914e16e2605344531d2
saken649 <>
2019-05-05 11:24:54 +0900

    デバッグモードの可変化

--- start_local_api.sh
+++ start_local_api.sh
@@ -5,9 +5,26 @@ if [ -z "${CODIC_TOKEN}" ]; then
     exit 1
 fi
 
-sam local start-api \
+IS_DEBUG=false
+while getopts d OPT
+do
+    case $OPT in
+        "d" ) IS_DEBUG=true ;;
+    esac
+done
+
+if [ ${IS_DEBUG} = true ]; then
+    echo -e ">>> Debug mode is enabled."
+    sam local start-api \
     --debug-port 5858 \
     --env-vars env.json \
     --parameter-overrides \
         ParameterKey=StageEnv,ParameterValue=local \
         && ParameterKey=CodicToken,ParameterValue=${CODIC_TOKEN}
+else
+    sam local start-api \
+        --env-vars env.json \
+        --parameter-overrides \
+            ParameterKey=StageEnv,ParameterValue=local \
+            && ParameterKey=CodicToken,ParameterValue=${CODIC_TOKEN}
+fi
